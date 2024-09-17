a15a11a12cdfed26f26e386a64ff0c36ca4513fa
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2024-09-17 21:29:06 +0200

    .

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// function emToPx(em) {
+//   const rootFontSize = parseFloat(
+//     getComputedStyle(document.documentElement).fontSize
+//   );
+//   return em * rootFontSize;
+// }
+// const howItWorksContainerSelector = '#ai-how-it-works .top-content .left'
+// const cards = document.querySelectorAll(`${howItWorksContainerSelector} .card`).length;
+// const lastCard = document.querySelector(
+//   `${howItWorksContainerSelector} .card:last-child`
+// );
+
+// // 75em w pikselach
+// const breakpoint = emToPx(75);
+
+// if (window.innerWidth >= breakpoint) {
+//   lastCard.style.gridColumn = `span ${cards - 1}`;
+// }
